01293e20a7c8f76294254c19d3535ff8e07f1385
= <margaryan.mher.28@gmail.com>
2016-11-15 00:12:28 +0400

    Whois

---
+++ whois.py
@@ -0,0 +1,10 @@
+import os
+
+
+# whois facebook.com
+# information about the website, author etc
+def get_whois(url):
+    command = 'whois ' + url
+    process = os.popen(command)
+    result = str(process.read())
+    return result
